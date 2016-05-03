ad5b986dc52e8de541c43fd16a8b344b5c436378
初コミット <yusakusugiyama@sugiyamatomosaku-no-MacBook-Pro.local>
2016-05-03 18:44:04 +0900

    ギャンブル依存症用スクレイピング完成(一部nullあり)

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -4,19 +4,26 @@ class Scraping
 #yahooニュースのギャンブル依存症カテゴリをスクレイピング
   def self.addictionnews
     agent = Mechanize.new
+    links = []
+
     page = agent.get("http://news.yahoo.co.jp/theme/04f6c6afdb151e6c6657/")
-    titles = page.search('div.mc a')
-    image_links = page.search('div.themeDetail p.thumbtop img')
-
-
-
-    image_links.each do |image_url|
-      puts image_url.get_attribute('src')
+    elements = page.search('div.mc a')
+    elements.each do |ele|
+      links << ele.get_attribute('href')
     end
 
-    titles.each do |title|
-      puts title.inner_text
+    links.each do |link|
+      page = agent.get(link)
+      title = page.at('div.hd h1').inner_text if page.at('div.hd h1')
+      text = page.at('p.ynDetailText').inner_text if page.at('p.ynDetailText')
+      image_url = page.at('.thumb a img').get_attribute('src') if page.at('.thumb a img')
+
+      topic = Topic.where(title: title,text: text,image_url: image_url).first_or_initialize
+      topic.genre = "addiction"
+      topic.save
     end
+
+   
 
   end
 
