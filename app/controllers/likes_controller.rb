04675a2ca5130d935761b5635b76c68e12ea09f6
初コミット <yusakusugiyama@sugiyamatomosaku-no-MacBook-Pro.local>
2016-05-03 22:08:12 +0900

    topicsコントローラーの編集とrender partial「_likes.html.erbの作成」

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,18 @@
 class LikesController < ApplicationController
+
   def index
+    @topics = Topic.limit(50).order('id DESC')
+    
   end
+
+  def create
+    @like = Like.create(topic_id: params[:topic_id])
+    @likes = Like.where(topic_id: params[:topic_id])
+    @topics = Topic.all
+  end
+
+  def destroy
+  end
+
+
 end
