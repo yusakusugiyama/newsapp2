8fdcab0f64873defcb973bde30feb2be318c2b1e
初コミット <yusakusugiyama@sugiyamatomosaku-no-MacBook-Pro.local>
2016-05-03 21:30:27 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503064118) do
+ActiveRecord::Schema.define(version: 20160503122430) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "topic_id",   limit: 4
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "topics", force: :cascade do |t|
     t.text     "title",      limit: 65535
