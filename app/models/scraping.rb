b0ced2519f08c255deee4b7c1ef8f1a2fe2845dd
初コミット <yusakusugiyama@sugiyamatomosaku-no-MacBook-Pro.local>
2016-05-03 19:46:53 +0900

    グノシースクレイピング途中

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,8 +1,7 @@
 class Scraping
 
-
 #yahooニュースのギャンブル依存症カテゴリをスクレイピング
-  def self.addictionnews
+  def self.addiction_topic_1
     agent = Mechanize.new
     links = []
 
@@ -20,14 +19,37 @@ class Scraping
 
       topic = Topic.where(title: title,text: text,image_url: image_url).first_or_initialize
       topic.genre = "addiction"
+      topic.url = link
       topic.save
     end
-
-   
-
   end
 
+#プログラミング情報(グノシーウェブカテゴリ)
+    def self.programming_topic_1
+    agent = Mechanize.new
+    links = []
+
+    page = agent.get("https://gunosy.com/tags/434")
+    elements = page.search('div.list_content div.list_title a')
+    elements.each do |ele|
+      links << ele.get_attribute('href')
+    end
+
+    links.each do |link|
+      page = agent.get(link)
+      title = page.at('h1.article_header_title').inner_text if page.at('h1.article_header_title')
+      text = page.at('p.ynDetailText').inner_text if page.at('p.ynDetailText')
+      image_url = page.at('.thumb a img').get_attribute('src') if page.at('.thumb a img')
+
+      topic = Topic.where(title: title,text: text,image_url: image_url).first_or_initialize
+      topic.genre = "addiction"
+      topic.url = link
+      topic.save
+    end
+  end
 
 end
+
+
 
 
