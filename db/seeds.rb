7208bac0bc4656fe11a1a9c8ec69858146f72860
初コミット <yusakusugiyama@sugiyamatomosaku-no-MacBook-Pro.local>
2016-05-08 22:28:46 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
- User.create(email: 'tech-camp@gmail.com', password: '12345678', nickname: 'Mako')
+require "csv"
+
+users_csv = CSV.readlines("db/companies.csv")
+users_csv.shift
+users_csv.each do |row|
+  User.create(email: row[1], nickname: row[14])
+end
